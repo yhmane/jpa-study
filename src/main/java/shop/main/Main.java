005dbd4861896496e85f4cc335cde96095d1c842
hwangyunho <hwang9yh@wemakeprice.com>
2021-08-11 19:31:39 +0900

    JPA 연관관계 추가

--- src/main/java/shop/main/Main.java
+++ src/main/java/shop/main/Main.java
@@ -5,8 +5,8 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-import shop.domain.Member;
 import shop.domain.Order;
+import shop.domain.OrderItem;
 
 public class Main {
 
@@ -18,11 +18,11 @@ public class Main {
         transaction.begin();
 
         try {
-            Order order = entityManager.find(Order.class, 1L);
-            Long memberId = order.getMemberId();
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
 
-            // member를 entity에서 바로 꺼내오지 않기 때문에 객체지향적이지 않다 -> table 중심의 entity 매핑 문제점
-            Member member = entityManager.find(Member.class, memberId);
+            entityManager.persist(order);
+
             transaction.commit();
         } catch (Exception e) {
             transaction.rollback();
